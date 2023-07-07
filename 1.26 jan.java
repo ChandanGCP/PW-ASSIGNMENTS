374ca3a6a4eae1f1818b6816134cb7fa32bb18be
ChandanGCP <codingworld32@gmail.com>
2023-07-07 21:42:38 +0530

    PW assignment 26 jan clear

---
+++ 1.26 jan.java
@@ -0,0 +1,19 @@
+Q1 ans- A programming language is a vocabulary and set of grammatical rules for instructing a computer or computing device to perform specific tasks. • The term programming language usually refers to high-level languages, such as BASIC, C, C++, COBOL, Java, FORTRAN, Ada, and Pascal.
+
+Q2 ans-  It is important to automate, collect, manage, calculate, and analyze the processing of data and information accurately. Programming helps create software and applications that help computer and mobile users in daily life.
+
+Q3 ans- Major Features of Java Programming Language
+Simple.
+Object-Oriented.
+Platform Independent.
+Portable.
+Robust.
+Secure.
+Interpreted.
+Multi-Threaded.
+
+Q4 ans- objects are the things you think about first in designing a program and they are also the units of code that are eventually derived from the process.
+
+Q5 ans- A class is a template definition of the methods and variables in a particular kind of object.
+
+Q6 ans- The Java main method is usually the first method you learn about when you start programming in Java because its the entry point for executing a Java program.
