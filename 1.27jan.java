068485fad61abe4799dcce5711df7a63b9bd2e6c
ChandanGCP <codingworld32@gmail.com>
2023-07-08 22:10:27 +0530

    This is 27 jan assignment

---
+++ 1.27jan.java
@@ -0,0 +1,85 @@
+1. What is statically typed and dynamically typed programming language ?
+Ans- i)Type Checking:
+Statically typed languages perform type checking at compile-time, where the types of variables are verified before the program is run. This early type checking helps catch potential errors before execution, improving the reliability and efficiency of the code. On the other hand, dynamically typed languages perform type checking at runtime, which allows for more flexibility but may result in type-related errors only being discovered during program execution.
+ii) Flexibility vs. Safety:
+Dynamically typed languages offer more flexibility because variables can hold values of different types and their types can change during runtime. This flexibility can make the code easier to write and read, especially for small and dynamic projects. Statically typed languages, on the other hand, provide stronger type safety by enforcing strict type rules. They prevent type-related errors and make it easier to understand the code by explicitly declaring variable types. Although this may require more upfront effort, it often leads to more maintainable and scalable codebases.
+These points highlight the trade-offs between flexibility and safety in programming languages, allowing developers to choose the type system that best suits their project requirements and coding preferences.
+
+2. What is the variable in java ?
+ans: Java variables: named storage for specific data types, declared with type and name, can be initialized and assigned values, and have scopes.
+
+3. How to assign a value to variable ?
+ans: To assign a value to a variable in Java, use the assignment operator (=). Syntax: variableName = value;. The assigned value must be compatible with the variable's declared type.
+
+4. What are primitive  datadype in  java?
+ans:In Java, primitive data types are predefined basic data types that represent the most fundamental and simple types of data. There are eight primitive data types:
+
+byte: Represents a 8-bit signed integer value.
+short: Represents a 16-bit signed integer value.
+int: Represents a 32-bit signed integer value.
+long: Represents a 64-bit signed integer value.
+float: Represents a 32-bit floating-point value.
+double: Represents a 64-bit floating-point value.
+boolean: Represents a boolean value, which can be either true or false.
+char: Represents a single 16-bit Unicode character.
+
+5.What are the identifeirs in  java?
+ans:In Java, identifiers are names used to identify variables, methods, classes, and other program entities. They must start with a letter, underscore, or dollar sign, and follow specific naming rules and conventions.
+
+6.List the operators in java ?
+ans: Here is a list of operators in Java:
+Arithmetic Operators:
+Addition (+)
+Subtraction (-)
+Multiplication (*)
+Division (/)
+Modulo (remainder) (%)
+Assignment Operators:
+
+Simple assignment (=)
+Addition assignment (+=)
+Subtraction assignment (-=)
+Multiplication assignment (*=)
+Division assignment (/=)
+Modulo assignment (%=)
+And assignment (&=)
+Or assignment (|=)
+XOR assignment (^=)
+Left shift assignment (<<=)
+Right shift assignment (>>=)
+Unsigned right shift assignment (>>>=)
+Increment/Decrement Operators:
+
+Increment (++)
+Decrement (--)
+Relational Operators:
+
+Equal to (==)
+Not equal to (!=)
+Greater than (>)
+Less than (<)
+Greater than or equal to (>=)
+Less than or equal to (<=)
+Logical Operators:
+
+Logical AND (&&)
+Logical OR (||)
+Logical NOT (!)
+Bitwise Operators:
+
+Bitwise AND (&)
+Bitwise OR (|)
+Bitwise XOR (^)
+Bitwise NOT (~)
+Left shift (<<)
+Right shift (>>)
+Unsigned right shift (>>>)
+Conditional (Ternary) Operator:
+
+Conditional expression (? :)
+Instanceof Operator:
+
+Tests whether an object is an instance of a specific class (instanceof)
+
+7. Explain about increment and decrement operators and give an examples .
+ans: The increment (++) and decrement (--) operators are unary operators used to increase or decrease the value of a variable by 1. They can be used as prefix or postfix operators and are commonly used in loops. Examples: ++count, count++, --count, count--.
